03f6fe300409ad0c03c25716dfa3ed51d7681dd7
tatitani <tatitani@naver.com>
2022-01-11 19:10:33 +0900

    validator, chalk 패키지 추가

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,8 +1,17 @@
-var fs = require("fs"); // 모듈 불러옴
+// var fs = require("fs"); // 모듈 불러옴
 
-// fs.writeFileSync("notes.txt", `This file was created by NodeJS`);
-// fs.appendFileSync(`notes.txt`, `추가 내용222'`);
+// const filePath = "notes-app/notes.txt";
+// fs.writeFileSync(filePath, `This file was created by NodeJS`);
+// fs.appendFileSync(filePath, `추가 내용222'`);
 
 const add = require("./util.js");
 const sum = add(1, 3);
 console.log(sum);
+
+const validator = require("validator");
+//import validator from "validator"; // Note : NodeJS는 commmonJS 기반이라 import, export 키워드 사용 불가
+console.log(validator.isEmail("woong@g.com"));
+
+const chalk = require("chalk");
+const greenMsg = chalk.green.bold("green txt");
+console.log(greenMsg);
