23f631c84f5908b38156426efdd1744dbcc8b028
tatitani <tatitani@naver.com>
2022-01-26 04:53:47 +0900

    404 에러페이지 적용

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,6 +1,7 @@
 const path = require("path")
 const express = require("express")
 const hbs = require("hbs")
+const { mainModule } = require("process")
 
 const app = express()
 
@@ -30,19 +31,41 @@ app.get("", (req, res) => {
 
 app.get("/about", (req, res) => {
   res.render("about", {
-    aboutText: "This is aboout Text",
     title: "About Page",
     name: "woong_about",
+    aboutText: "This is aboout Text",
   })
 })
 
 app.get("/weather", (req, res) => {
-  res.send({
+  res.render("index", {
     forecast: "It is snowing",
-    lcoation: "mulgeum",
+    location: "mulgeum",
+    name: "woong_weather",
+  })
+})
+
+// 해당 페이지 없는 경우
+// get으로 페이지들을 모두 지정하고 마지막에 위치해야 정상 동작
+
+app.get("/about/*", (req, res) => {
+  res.render("404", {
+    title: "Error about Page",
+    name: "woong_error_about",
+    errorMessage: "404 에러임",
+  })
+})
+
+app.get("*", (req, res) => {
+  res.render("404", {
+    title: "Error Page",
+    name: "woong_error",
+    errorMessage: "404 에러임",
   })
 })
 
 app.listen(3000, () => {
   console.log("Server is up on port 3000.")
 })
+
+// 터미널 명령어 : nodemon src/app.js js, hbs
