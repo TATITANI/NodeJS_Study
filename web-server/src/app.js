bd4dc41e53678728398530d345bbcc1ee046c72c
tatitani <tatitani@naver.com>
2022-01-26 00:27:18 +0900

    템플릿 재사용

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,13 +1,26 @@
 const path = require("path")
 const express = require("express")
+const hbs = require("hbs")
 
 const app = express()
+
+//define paths for Express config
 const publicDirectoryPath = path.join(__dirname, "../public")
+const viewPath = path.join(__dirname, "../templates/views")
+const partialPath = path.join(__dirname, "../templates/partials")
+
 console.log(__dirname)
 console.log(publicDirectoryPath)
 
+// Setup handlerbars engine and views location
 app.set("view engine", "hbs")
+app.set("views", viewPath)
+// 템플릿 재사용
+hbs.registerPartials(partialPath)
+
+// Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
+
 app.get("", (req, res) => {
   res.render("index", {
     title: "Weather App",
@@ -17,6 +30,7 @@ app.get("", (req, res) => {
 
 app.get("/about", (req, res) => {
   res.render("about", {
+    aboutText: "This is aboout Text",
     title: "About Page",
     name: "woong_about",
   })
