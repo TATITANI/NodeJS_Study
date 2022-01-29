ff5bd23d74f1d8174c4ed4581de32a1d5bc2ea80
tatitani <tatitani@naver.com>
2022-01-30 01:19:06 +0900

    http request with fetch

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,12 @@
 console.log("Client side javascript file is loaded!");
+
+fetch('http://localhost:3000/weather?address=korea').then((response) => {
+    response.json().then((data) => {
+        if (data.error) {
+            console.log(data.error)
+        } else {
+            console.log(data.address)
+            console.log(data.forecast)
+        }
+    })
+})
