e59c87400c1966b2857d473198aba95a516b43f6
tatitani <tatitani@naver.com>
2022-02-13 06:14:19 +0900

    날씨 출력 단락 크기 설정

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -5,7 +5,7 @@ const search = document.querySelector("input")
 
 const messageWeather = document.getElementById("msg-weather")
 const messageInfo = document.getElementById("msg-info")
-messageWeather.textContent = "..."
+messageWeather.textContent = ""
 messageInfo.textContent = "Using NodeJS"
 const results = document.getElementById('result');
 var xhr = new XMLHttpRequest()
@@ -27,22 +27,16 @@ geocoder.on("result", (e) => {
   const lat = coordinates[1]
 
   const address = document.getElementById("geocoder").getElementsByClassName('mapboxgl-ctrl-geocoder--input')[0].value
-  var data = {
+  const data = {
     address : address,
     lat : lat,
     long : long
   }
-
-  var data = {
-    address : address,
-    lat : lat,
-    long : long,
-  }
-
   xhr.open("POST", "http://localhost:3000/")
   xhr.setRequestHeader("Content-type", "application/json") // 콘텐츠 타입을 json으로
   // 'application/x-www-form-urlencoded' : &으로 분리되고, "=" 기호로 값과 키를 연결하는 key-value tuple로 인코딩되는 값
   xhr.send(JSON.stringify(data)) // 데이터 전송
+  messageWeather.textContent = `loading...`
 
   // 응답이 왔을 때 콜백 실행
   xhr.addEventListener("load", function () {
