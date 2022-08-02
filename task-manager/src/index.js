8ab2436610e531238ec0e4addcaadb91ae4bac8f
tatitani <tatitani@naver.com>
2022-08-02 23:01:15 +0900

    patch

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -66,6 +66,32 @@ app.get('/tasks/:id', async (req, res) => {
     }
 })
 
+app.patch('/tasks/:id', async (req, res) => {
+    const updates = Object.keys(req.body)
+    console.log(`keys : ${updates}`) // 리퀘스트의 키값
+
+    const allowedUpdates = ['description' ,'completed']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+    if(!isValidOperation){
+        return res.status(400).send({error : 'invalid key'})
+    }
+    
+    try{
+        // new : ture => 변경한 데이터 반환
+        // runValidators : 스키마에 정의한 validation 동작
+        const task = await Task.findByIdAndUpdate(req.params.id, req.body, {new : true, runValidators :true})
+
+        if(!task){
+            return res.status(404).send()
+        }
+
+        res.send(task)
+
+    }catch(e){
+        res.status(400).send(e)
+    }
+
+})
 
 app.get('/user', async (req, res) => {
     // 모든 document 조회
@@ -94,10 +120,36 @@ app.get('/user/:id', async (req, res) => {
     
 })
 
+app.patch('/user/:id', async (req, res) =>{
+    const updates = Object.keys(req.body)
+    console.log(`keys : ${updates}`) // 리퀘스트의 키값
+
+    const allowedUpdates = ['name' ,'email', 'password', 'age']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+    if(!isValidOperation){
+        return res.status(400).send({error : 'invalid key'})
+    }
+    
+    try{
+        // new : ture => 변경한 데이터 반환
+        // runValidators : 스키마에 정의한 validation 동작
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, {new : true, runValidators :true})
+        if(!user){
+            return res.status(404).send()
+        }
+        res.send(user)
+
+    }catch(e){
+        res.status(400).send(e)
+    }
+})
+
+
 
 app.listen(port, () => {
     console.log(`Server is up on port ${port}`)
 })
+
 
 // MongoDB 실행 : D:/mongodb/bin/mongod.exe --dbpath=D:/mongodb-data
 //실행 명령어 : nodemon src/index.js
