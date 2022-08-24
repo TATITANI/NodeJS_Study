9f76b6dab3f1c24ffa288278b077bf5f41a2e911
tatitani <tatitani@naver.com>
2022-08-24 21:08:49 +0900

    socketio 송수신

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -1,16 +1,40 @@
 const express = require('express')
+const http = require('http')
+const path = require("path")
+const socketio = require('socket.io')
 
 const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
+
+const port = process.env.PORT
+const publicDirectoryPath = path.join(__dirname, "../public")
+
 app.use(express.json())
 
-const publicDirectoryPath = path.join(__dirname, "../public")
 // Setup static directory to server  
 // static 미들웨어는 특정 디렉토리 아래에 있는 폴더와 파일들을 특정 경로로 접근할 수 있도록 만들어준다.
 // => partial 사용 가능
 app.use(express.static(publicDirectoryPath))
 
-const port = process.env.PORT
-app.listen(port, () => {
+count = 0
+io.on('connection', (socket) => {
+    console.log("New Websocket connection")
+
+    //송신
+    socket.emit('countUpdated',count)
+    //수신
+    socket.on('increment', () =>{
+        count++
+
+        //단일 커넥션에 전송
+        // socket.emit('countUpdated', count)
+        // 모든 커넥션에 전송
+        io.emit('countUpdated', count)
+    })
+})
+
+server.listen(port, () => {
     console.log(`Server is up on port ${port}`)
 })
 
